870b84003177c67e97515a3125d29c1639d4de12
Gabriel Mazzei <gmazzei91@gmail.com>
2018-08-21 15:12:15 -0300

    Text fields underlined

--- WBooks/CustomViews/SimpleTextField.swift
+++ WBooks/CustomViews/SimpleTextField.swift
@@ -28,6 +28,18 @@ private extension SimpleTextField {
         font = UIFont.systemFont(ofSize: 19)
         borderStyle = UITextBorderStyle.none
         backgroundColor = UIColor.clear
+        setBottomBorder()
+    }
+    
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
     }
 }
 
